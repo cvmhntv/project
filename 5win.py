cca75370fbfc6de353a7b19e012b7ef3f411ddff
cvmhntv <noncensy@gmail.com>
2026-05-28 16:02:35 +0800

    Add fifth_win class for new PyQt5 window

---
+++ 5win.py
@@ -0,0 +1,28 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
+
+class fifth_win(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+    def set_appear(self):
+        self.setWindowTitle(text3_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+        self.outro = QLabel(text4_outro)
+        self.buttonn = QPushButton(text5_button)
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.outro)
+        self.layout.addWidget(self.buttonn)
+        self.setLayout(self.layout)
+    def connects(self):
+        self.button.clicked.connect(self.next_click)
+    def next_click(self):
+        self.hide()
+app = QApplication([])
+mw = MainWin()
+app.exec_()
