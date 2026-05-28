3919d63fab38832bbae3c4b7675433420567c808
cvmhntv <noncensy@gmail.com>
2026-05-28 18:33:19 +0800

    Update 1win.py

--- 1win.py
+++ 1win.py
@@ -1,6 +1,6 @@
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton
 from instr import *
+from win2 import second_win
 
 class first_win(QWidget):
     def __init__(self):
@@ -13,8 +13,10 @@ class first_win(QWidget):
         self.setWindowTitle(text3_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
+
     def initUI(self):
         self.intro = QLabel(text1_intro)
+        self.intro.setWordWrap(True)
         self.button = QPushButton(text2_button)
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.intro)
@@ -25,7 +27,7 @@ class first_win(QWidget):
     def next_click(self):
         self.hide()
         self.tw = second_win()
-
-app = QApplication([])
-mw = first_win()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication([])
+    mw = first_win()
+    app.exec_()
