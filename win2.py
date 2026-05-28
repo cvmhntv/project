51d1bc8e1f209b64c6f85bd89fc6caa1494bf228
cvmhntv <noncensy@gmail.com>
2026-05-28 19:03:12 +0800

    Update win2.py

--- win2.py
+++ win2.py
@@ -13,7 +13,6 @@ class second_win(QWidget):
     def set_appear(self):
         self.setWindowTitle(text3_title)
         self.resize(win_width, win_height)
-        self.move(win_x, win_y)
     def initUI(self):
         self.question11 = QLabel(question1)
         self.question11.setWordWrap(True)
@@ -38,11 +37,11 @@ class second_win(QWidget):
         self.setLayout(self.layout_main)
     def show_win(self):
         self.win = QMessageBox()
-        self.win.setText('верно')
+        self.win.setText('Верно')
         self.win.exec_()
     def show_lose(self):
         self.lose = QMessageBox()
-        self.lose.setText('неверно')
+        self.lose.setText(f'Неверно\nПравильный ответ: {answer1_3}')
         self.lose.exec_()
     def connects(self):
         self.answer11.clicked.connect(self.show_lose)
